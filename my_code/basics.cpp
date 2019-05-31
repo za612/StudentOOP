161d8b6142402b0e851913fc8b70db2fc0a19101
za612 <35902898+za612@users.noreply.github.com>
2019-05-31 12:51:45 -0400

    new

--- my_code/basics.cpp
+++ my_code/basics.cpp
@@ -1,1 +1,112 @@
-a
+#include <iostream>
+#include <string>
+#include <vector>
+#include "basics.h"
+
+using namespace std;
+
+/*
+ * increment arg passed by copy
+ * */
+void inc_pass_copy(int j) {
+	++j;
+}
+
+/*
+ * increment arg passed by reference
+ * */
+void inc_pass_ref(int& j) {
+	++j;
+}
+
+/*
+ * Fill a vector with n elements from 1 to n.
+ * 1st item == 1, 2nd item == 2, etc.
+ * 0 filled for you.
+ * Use `push_back()` to fill the vector.
+ * */
+void fill_vector(vector<int>& v, int n) {
+	for (int i = 1; i <= n; ++i) {
+		v.push_back(i);
+	}
+}
+
+/*
+ * Print all elements in a vector. Use a ranged for!
+ * */
+void print_vector(const vector<int>& v) {
+	for (int i : v) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
+
+/*
+ * Print a pair of coordinates.
+ * */
+void print_coords(coord pos) {
+	cout << "x = " << pos.x << ", y = " << pos.y << endl;
+}
+
+/*
+ * Square an integer.
+ * */
+int square(int n) {
+	return n * n;
+}
+
+/*
+ * Square a double.
+ * */
+double square(double d) {
+	return d*d;
+}
+
+/*
+ * Get the absolute value of a number.
+ * */
+int my_abs(int n) {
+	return abs(n);
+}
+
+/*
+ * Sum from 0 to n using a `while` loop.
+ * */
+int sum(int n) {
+	int result = 0;
+	int i = 0;
+	while (i < n) {
+		result += i;
+		i++;
+	}
+	return result;
+}
+
+/*
+ * Sum from 0 to n using a `for` loop.
+ * */
+int sum2(int n) {
+	int result = 0;
+	for (int i = 0; i < n; ++i) {
+		result += i;
+	}
+	return result;
+}
+
+/*
+ * say() is built for you!
+ * */
+void say(std::string s) {
+	cout << s << endl;
+}
+
+
+/*
+ * Use a ranged for to pull a string apart into its bits.
+ * */
+void pull_apart_string(std::string s) {
+	for (char i : s) {
+		cout << i << ' ';
+	}
+	cout << endl;
+}
--- tests/test_basics.cpp
+++ tests/test_basics.cpp
@@ -1,1 +1,63 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../my_code/basics.h"
 
+using namespace std;
+
+int main() {
+
+    say("Hello!");
+
+    int j = 8;
+    inc_pass_copy(j);
+    cout << "j = " << j << endl;
+    inc_pass_ref(j);
+    cout << "j = " << j << endl;
+    // Put this assertion back once you've written code:
+    // assert(j == 9);
+
+    int test_num = 10;
+    int ret = square(test_num);
+    assert(ret == (test_num * test_num));
+
+    double test_dbl = 2.5;
+    double dret = square(test_dbl);
+    cout << "2.5 squared = " << dret << endl;
+    assert(dret == (test_dbl * test_dbl));
+    cout << "Int value of dret = " << (int)dret << endl;
+
+    ret = my_abs(test_num);
+    assert(ret == test_num);
+    ret = 77;
+    ret = my_abs(-test_num);
+    assert(ret == test_num);
+
+    ret = sum(test_num);
+    assert(ret == 45);
+    ret = sum2(test_num);
+    assert(ret == 45);
+
+    vector<int> v = { 0 };
+    fill_vector(v, test_num);
+    print_vector(v);
+    cout << "v[1] = " << v[1] << endl;
+
+    coord pos;
+    pos.x = 7;
+    pos.y = 8;
+    print_coords(pos);
+
+    char c = 'A';
+    string s = "C++ strings are collections of chars.";
+    s += '!';
+    pull_apart_string(s);
+    assert(c < s[4]);
+    c++;
+    cout << "c = " << c << endl;
+    cout << "Int val of c = " << (int)c << endl;
+
+    say("Goodbye!");
+    return 0;
+}
