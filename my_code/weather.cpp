7f00f8d71dc71323ac17ede06b31df5d7777a326
za612 <35902898+za612@users.noreply.github.com>
2019-06-05 12:11:20 -0400

    Create weather.cpp

---
+++ my_code/weather.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "weather.h"
+
+using namespace std;
+
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(string nm) : station_nm(nm) {
+}
