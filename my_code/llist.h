5fb725ebebeb6c2a617ef86ab9f3a3d50a16e801
za612 <35902898+za612@users.noreply.github.com>
2019-06-24 11:39:44 -0400

    Create llist.h

---
+++ my_code/llist.h
@@ -0,0 +1,56 @@
+#ifndef LINKED_H
+#define LINKED_H
+
+#include <iostream>
+
+
+/*
+ * The struct `Node`, which is the building block of our linked list code.
+ * */
+struct Node {
+	Node(int d, Node* n = nullptr) : data(d), next(n) {}
+	int data;
+	Node* next;
+};
+
+
+/*
+ * The various functions that can operate on our linked lists:
+ * */
+std::ostream& operator<<(std::ostream& os, const Node* nd);
+void print_list(std::ostream& os, const Node* curr);
+Node* last(Node* head);
+void add_at_end(Node*& head, int d);
+void add_at_front(Node*& head, int d);
+
+/*
+ * These are coming:
+*/
+
+/*
+ * Delete the first node and attach head to the 2nd node:
+ * */
+bool del_head(Node*& head);
+
+/*
+ * Delete the last node and set prev->next to nullptr:
+ * */
+bool del_tail(Node*& head);
+
+/*
+ * Duplicate the entire list -- you must not share memory!
+ * */
+Node* duplicate(Node* head);
+
+/*
+ * Reverse the list: return a brand new list with everything reversed.
+ * */
+Node* reverse(Node* curr, Node* new_next = nullptr);
+
+/*
+ * Join two lists: tack list 2 onto the end of list 1:
+ * Use existing memory.
+ * */
+Node* join(Node*& list1, Node* list2);
+
+#endif
