f3f0004732a45b73faac04b5b635aa17add925d5
za612 <35902898+za612@users.noreply.github.com>
2019-06-27 21:46:44 -0400

    Update tvector.h

--- my_code/tvector.h
+++ my_code/tvector.h
@@ -1,61 +1,127 @@
+
 const int DEF_CAPACITY = 10;
 const int CAPACITY_MULT = 2;
 
-template<typename T>
-class MyVec {
+template <typename T>
+class Iterator {
+	/*
+	 * This class supports ranged for loops.
+	 * It includes:
+	 * Iterator(T*)
+	 * operator++
+	 * operator*
+	 * operator!=
+	 * */
+	friend bool operator!=(Iterator& rhs, Iterator& lhs) {
+		return rhs.tptr == lhs.tptr;
+	}
+
 public:
-	template<typename T>
-	class Iterator {
-		/*
-		 * This class supports ranged for loops.
-		 * It includes:
-		 * Iterator(int*)
-		 * operator++
-		 * operator*
-		 * operator!=
-		 * */
-		friend bool operator!=(Iterator& rhs, Iterator& lhs) {
-			return lhs.iptr != rhs.iptr;
-		}
+	Iterator(T* tp) : tptr(tp) {}
 
-	public:
-		Iterator(T* ip) : iptr(ip) {}
+	Iterator& operator++() {
+		tptr++;
+		return *this;
+	}
 
-		Iterator& operator++() {
-			++iptr;
-			return *this;
-		}
-
-		T& operator*() { return *iptr; }
-
-	private:
-		T* iptr;
-	};
-
-	MyVec();
-	MyVec(int sz, T val);
-
-	// copy control:
-	MyVec(const MyVec& v2);
-	~MyVec();
-	MyVec& operator=(const MyVec& v2);
-
-	void push_back(T val);
-	int size() const { return sz; }
-	T operator[](int i) const;
-	T& operator[](int i);
-	Iterator<T> begin() const;
-	Iterator<T> end() const;
+	T operator*() { return *tptr; }
 
 private:
-	void copy(const MyVec& v2);
+	T* tptr;
+};
+
+
+template <typename T>
+class MyVec {
+public:
+	MyVec() : sz(0), capacity(DEF_CAPACITY) {
+		data = new T[DEF_CAPACITY];
+	}
+
+	MyVec(int sz, T val) : sz{ sz } {
+		capacity = sz * 2;
+		data = new T[capacity];
+		for (int i = 0; i < sz; i++) {
+			data[i] = val;
+		}
+	}
+
+	// copy control:
+	MyVec(const MyVec<T>& v2) {
+		copy(v2);
+	}
+
+	~MyVec() { delete[] data; }
+
+	MyVec& operator=(const MyVec& v2) {
+		if (this != &v2) {
+			delete[] data;
+			copy(v2);
+		}
+		return *this;
+	}
+
+
+	/*
+	 * Puts an element at the back of a vector.
+	 * */
+	void push_back(T val) {
+		sz++;
+		if (sz > capacity) {
+			std::cout << "Increasing capacity\n";
+			T* old_data = data;
+			data = new T[capacity * CAPACITY_MULT];
+			for (int i = 0; i < sz; i++) {
+				data[i] = old_data[i];
+			}
+			capacity *= CAPACITY_MULT;
+			delete[] old_data;
+		}
+		data[sz - 1] = val;
+	}
+
+	int size() const { return sz; }
+
+	T operator[](int i) const {
+		return data[i];
+	}
+
+	T& operator[](int i) {
+		return data[i];
+	}
+
+	Iterator<T> begin() const { return Iterator<T>(data); }
+	Iterator<T> end() const { return Iterator<T>(data + size()); }
+
+private:
+	void copy(const MyVec<T>& v2) {
+		sz = v2.sz;
+		capacity = v2.capacity;
+		data = new T[capacity];
+		for (int i = 0; i < sz; i++) {
+			data[i] = v2.data[i];
+		}
+	}
+
 	T* data;
 	int sz;
 	int capacity;
 };
 
-template<typename T>
-void print_vector(const MyVec<T>& v);
 
-template<typename T>
-bool operator==(MyVec<T>& v1, MyVec<T>& v2);
+template <typename T>
+void print_vector(const MyVec<T>& v) {
+	for (int i = 0; i < v.size(); i++)
+		std::cout << v[i] << ", ";
+	std::cout << std::endl;
+}
+
+template <typename T>
+bool operator==(MyVec<T>& v1, MyVec<T>& v2) {
+	if (v1.size() != v2.size()) return false;
+
+	for (int i = 0; i < v1.size(); i++) {
+		if (v1[i] != v2[i]) return false;
+	}
+	return true;
+}
